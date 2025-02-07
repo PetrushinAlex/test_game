4aa9aad4fc0689f835139286322914d1c5a74107
Aleksandr Petrushin <jacka.92@mail.ru>
2025-02-07 17:55:53 +0300

    Initial commit

---
+++ game.py
@@ -0,0 +1,15 @@
+from tkinter import *
+import random as rm
+
+
+class Main(Frame):
+    def __init__(self, root):
+        super(Main, self).__init__(root)
+        self.startUI()
+
+    def startUI(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
