0940623d36f31cf4b5b95b834577282d66e043c3
Aleksandr Petrushin <jacka.92@yandex.ru>
2025-02-15 13:48:14 +0300

    correct game field

--- game.py
+++ game.py
@@ -9,34 +9,34 @@ class Main(Frame):
 
     def startUI(self):
         btn = Button(
-            root, text="Камень", font=("Times New Roman", 20),
+            root, text='Камень', font=('Arial', 24),
             command=lambda x=1: self.btn_click(x)
         )
         btn2 = Button(
-            root, text="Ножницы", font=("Times New Roman", 20),
+            root, text='Ножницы', font=('Arial', 24),
             command=lambda x=2: self.btn_click(x)
         )
         btn3 = Button(
-            root, text="Бумага", font=("Times New Roman", 20),
+            root, text='Бумага', font=('Arial', 24),
             command=lambda x=3: self.btn_click(x)
         )
 
-        btn.place(x=40, y=100, width=120, height=50)
-        btn2.place(x=240, y=100, width=120, height=50)
-        btn3.place(x=480, y=100, width=120, height=50)
+        btn.place(x=250, y=300, width=200, height=80)
+        btn2.place(x=500, y=300, width=200, height=80)
+        btn3.place(x=750, y=300, width=200, height=80)
 
-        self.lbl = Label(root, text="Начало игры!", bg="#FFF", font=("Times New Roman", 21, "bold"))
-        self.lbl.place(x=150, y=25)
+        self.lbl = Label(root, text='Начало игры!', bg="#FFF", font=('Arial', 36, 'bold'))
+        self.lbl.place(x=440, y=25)
 
         self.win = self.drow = self.lose = 0
 
         self.lbl2 = Label(
-            root, justify="left", font=("Times New Roman", 13),
-            text=f"Побед: {self.win}\nПроигрышей:"
-                 f" {self.lose}\nНичей: {self.drow}",
+            root, justify='left', font=('Arial', 16),
+            text=f'Побед: {self.win}\nПроигрышей:'
+                 f' {self.lose}\nНичей: {self.drow}',
             bg="#FFF"
         )
-        self.lbl2.place(x=5, y=5)
+        self.lbl2.place(x=15, y=15)
 
     def btn_click(self, choise):
         pass
@@ -44,7 +44,7 @@ class Main(Frame):
 
 if __name__ == '__main__':
     root = Tk()
-    root.geometry('1200x1400+200+200')
+    root.geometry('1200x800+200+200')
     root.title('Камень, ножницы, бумага')
     root.resizable(False, False)
     root['bg'] = '#90EE90'
